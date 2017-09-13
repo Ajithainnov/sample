c561b0980f31e764d1c909efc6918307576b2d19
Ajithainnov <ajithaanbazhagan97@gmail.com>
2017-09-13 16:50:47 +0530

    Create palin.java

---
+++ palin.java
@@ -0,0 +1,19 @@
+import io.*;
+public class palin 
+{
+		public static void main(String[] args) 
+    {
+     String input="abdmadamcbc";
+     for(int i=0;i<input.length();i++)
+     {
+    	 for(int j=i+1;j<input.length();j++)
+       {
+    		 StringBuffer buffer=new StringBuffer(input.substring(i,j+1));
+    		 if((buffer.reverse().toString()).equals(input.substring(i,j+1)))
+         {
+    			 System.out.println(input.substring(i,j+1));
+    		 }
+    	 }
+     }
+	}
+  }
